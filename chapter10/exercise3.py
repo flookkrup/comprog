f5badd58ee0ceb6b5f900d9191f9c1ced6b3aa18
ratchanonp <qq.panmas@gmail.com>
2021-11-06 16:45:06 +0700

    :pencil: Check No Bus go to destination

--- chapter10/exercise3.py
+++ chapter10/exercise3.py
@@ -1,12 +1,18 @@
-nBus = int(input("Enter n: "))
+nLine = int(input("Enter n: "))
 lines = {}
 
-for i in range(1, nBus + 1):
+for i in range(1, nLine + 1):
     lineNumber, destination = map(str, input(f"Enter bus no. and destination {i}: ").split(", "))
+    
     if destination in lines:
         lines[destination].append(lineNumber)
     else:
         lines[destination] = [lineNumber]
 
 destination = input("Enter your destination: ")
-print(f"You can get bus no. {' '.join(lines[destination])}")
+
+if destination in lines:
+    print(f"You can get bus no. {' '.join(lines[destination])}")
+else:
+    print(f"No buses go to {destination}")
+
